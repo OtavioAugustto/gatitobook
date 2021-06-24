35dcf332db66f3fef7e689875c21a5b0ed6d49dc
OtavioAugustto <otavioaugustto93@gmail.com>
2021-06-24 16:49:38 -0300

    Create Login Component and Define Interface

--- gatitobook/src/app/home/home-routing.module.ts
+++ gatitobook/src/app/home/home-routing.module.ts
@@ -1,11 +1,18 @@
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    children:[
+      {
+        path:'',
+        component: LoginComponent,
+      }
+    ]
   },
 ];
 
