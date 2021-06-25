a94e4104cfd4d178aa03e728863a3d8d49f0e49c
OtavioAugustto <otavioaugustto93@gmail.com>
2021-06-25 17:28:43 -0300

    Finish Auth Service and Create Animals module and component

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,15 +1,33 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
+  constructor(
+    private authService: AutenticacaoService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    //consumindo o serviço de autenticação com obserbable
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+        //redurecionando usando a rota dentro da aplicação
+        this.router.navigate(['animais'])
+      },
+      (error) => {
+        alert('Usuário ou senha inválido');
+        console.log(error);
+      }
+    );
   }
-
 }
