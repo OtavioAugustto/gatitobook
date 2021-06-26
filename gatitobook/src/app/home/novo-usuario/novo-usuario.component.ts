d44689a68deb20163a2720fe0a79ec49d17d3a5a
OtavioAugustto <otavioaugustto93@gmail.com>
2021-06-26 20:02:08 -0300

    Create metohd add new user and reactform

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,15 +1,31 @@
+import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
 
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  styleUrls: ['./novo-usuario.component.css'],
 })
 export class NovoUsuarioComponent implements OnInit {
-
-  constructor() { }
+  novoUsuarioForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private NovoUsuarioService: NovoUsuarioService
+  ) {}
 
   ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: [''],
+    });
   }
 
+  cadastrar(){
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
+  }
 }
