0bab6b232456c241d4dc6a7ee5124d0b94a997eb
OtavioAugustto <otavioaugustto93@gmail.com>
2021-06-28 17:21:57 -0300

    Create Header Component

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,6 +1,8 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -8,12 +10,26 @@ import { Observable } from 'rxjs';
 export class AutenticacaoService {
   private urlLogin = 'http://localhost:3000/user/login';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  autenticar(usuario: string, senha: string): Observable<any> {
-    return this.httpClient.post(this.urlLogin, {
-      userName: usuario,
-      password: senha,
-    });
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        this.urlLogin,
+        {
+          userName: usuario,
+          password: senha,
+        },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
   }
 }
