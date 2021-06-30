7364678412c5e6f9de1ecc782724c2a37d985534
OtavioAugustto <otavioaugustto93@gmail.com>
2021-06-30 16:41:37 -0300

    Create Details component and Interceptors

--- gatitobook/src/app/animais/animais-routing.module.ts
+++ gatitobook/src/app/animais/animais-routing.module.ts
@@ -1,12 +1,17 @@
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ListaAnimaisComponent,
   },
+  {
+    path:':animalId',
+    component:DetalheAnimalComponent
+  }
 ];
 
 @NgModule({
---
+++ gatitobook/src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -0,0 +1,24 @@
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Animal } from '../animais';
+import { AnimaisService } from '../animais.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-detalhe-animal',
+  templateUrl: './detalhe-animal.component.html',
+  styleUrls: ['./detalhe-animal.component.css'],
+})
+export class DetalheAnimalComponent implements OnInit {
+  animalId!: number;
+  animal$!: Observable<Animal>;
+  constructor(
+    private animaisService: AnimaisService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.animalId = this.activatedRoute.snapshot.params.animalId;
+    this.animal$ = this.animaisService.buscaPorId(this.animalId);
+  }
+}
