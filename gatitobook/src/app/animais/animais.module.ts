9c5e6911bbad25a8d7337e1ae05f0531abd679e8
OtavioAugustto <otavioaugustto93@gmail.com>
2021-07-01 18:17:55 -0300

    Create Comments Component

--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -1,3 +1,5 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { MensagemModule } from './../componentes/mensagem/mensagem.module';
 import { CartaoModule } from './../componentes/cartao/cartao.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -7,19 +9,22 @@ import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
-
+import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
 
 @NgModule({
   declarations: [
     ListaAnimaisComponent,
     AnimalComponent,
     GradeFotosAnimaisComponent,
-    DetalheAnimalComponent
+    DetalheAnimalComponent,
+    ComentariosComponent,
   ],
   imports: [
     CommonModule,
     AnimaisRoutingModule,
-    CartaoModule
-  ]
+    CartaoModule,
+    MensagemModule,
+    ReactiveFormsModule,
+  ],
 })
-export class AnimaisModule { }
+export class AnimaisModule {}
