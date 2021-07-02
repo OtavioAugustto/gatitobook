ed5e3568fb8a8cd476b91be3c88e05a30a125248
OtavioAugustto <otavioaugustto93@gmail.com>
2021-07-02 19:04:53 -0300

    Create Menu Component

--- gatitobook/src/app/componentes/cabecalho/cabecalho.module.ts
+++ gatitobook/src/app/componentes/cabecalho/cabecalho.module.ts
@@ -1,17 +1,12 @@
+import { MenuModule } from './../menu/menu.module';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabecalhoComponent } from './cabecalho.component';
 
-
-
 @NgModule({
-  declarations: [
-    CabecalhoComponent
-  ],
-  imports: [
-    CommonModule, RouterModule,
-  ],
-  exports:[CabecalhoComponent],
+  declarations: [CabecalhoComponent],
+  imports: [CommonModule, RouterModule, MenuModule],
+  exports: [CabecalhoComponent],
 })
-export class CabecalhoModule { }
+export class CabecalhoModule {}
